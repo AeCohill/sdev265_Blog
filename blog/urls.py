8ad86efb2c4959a25cd22023a81303ddbb08afdc
Adam Cohill <adamcohill16@gmail.com>
2024-07-08 19:21:06 -0400

    first

---
+++ blog/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+from django.contrib.auth import views as auth_views
+from . import views
+
+urlpatterns = [
+    path('login/', views.CustomLoginView.as_view(), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('register/', views.register, name='register'),
+    path('posts/', views.post_list, name='post_list'),
+    path('author/<int:author_id>/', views.author_detail, name='author_detail'),
+    path('upvote/<int:post_id>/', views.upvote_post, name='upvote_post'),
+    path('downvote/<int:post_id>/', views.downvote_post, name='downvote_post'),
+    path('post/new/', views.post_new, name='post_new'),
+    path('post/edit/<int:pk>/', views.post_edit, name='post_edit'),
+]
